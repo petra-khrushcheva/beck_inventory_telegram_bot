7277e9ce6a21154b329a37cb1f3442dd51efd7f1
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-28 18:35:53 +0500

    add get test result func

--- src/bot/utils.py
+++ src/bot/utils.py
@@ -1,2 +1,14 @@
 async def get_test_result(score) -> str:
-    pass
+    if 0 <= score <= 9:
+        return f"{score} баллов. Отсутствие депрессивных симптомов."
+    elif 10 <= score <= 18:
+        return f"{score} баллов. Субдепрессия, умеренная депрессия."
+    elif 19 <= score <= 29:
+        return f"{score} баллов. Выраженная депрессия средней тяжести."
+    elif 30 <= score <= 63:
+        return f"{score} баллов. Тяжелая депрессия."
+    else:
+        return (
+            "Кажется при работе теста произошла ошибка. Пожалуйста, "
+            "напишите об этом разработчикам: @FakeTelegramUsername"
+            )
